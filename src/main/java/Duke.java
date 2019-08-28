524b799a6ce0f92541c3df9b96daf488b4b1a580
Geek <ygeek@gmail.com>
2019-08-28 14:58:42 +0800

    Level-5 Error handling, used Exception to deal with few error - task type without description, done without index, and no task type

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,46 +13,79 @@ public class Duke {
         String input = scanner. nextLine();
         ArrayList<Task> tasks = new ArrayList();
         while (!input.equals("bye")) {
-            switch(input) {
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int x = 0; x < tasks.size(); x++) {
-                        System.out.println(x + 1 + "." + tasks.get(x).toString());
+            try {
+                switch (input) {
+                    case "list":
+                        System.out.println("Here are the tasks in your list:");
+                        for (int x = 0; x < tasks.size(); x++) {
+                            System.out.println(x + 1 + "." + tasks.get(x).toString());
+                        }
+                        break;
+                    default:
+                        if (input.substring(0, 5).equals("done ")) {
+                            String number = input.substring(5);
+                            int index = Integer.parseInt(number);
+                            tasks.get(index - 1).markAsDone();
+                            System.out.println("Nice! I've marked this task as done:\n" + tasks.get(index - 1).getStatusIcon() + tasks.get(index - 1).getName());
+                        }
+                        else if (input.substring(0, 5).equals("todo ")){
+                            String details = input.substring(5);
+                            System.out.println(details);
+                            Todo todo = new Todo(details);
+                            tasks.add(todo);
+                            System.out.println("Got it. I've added this task:\n" + todo.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        }
+                        else if (input.substring(0, 6).equals("event ")) {
+                            String details = input.substring(6);
+                            int delimiter = details.indexOf("/");
+                            String task = details.substring(0, delimiter);
+                            String time = details.substring(delimiter + 4);
+                            Event event = new Event(task, time);
+                            tasks.add(event);
+                            System.out.println("Got it. I've added this task:\n" + event.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        }
+                        else if (input.substring(0, 9).equals("deadline ")) {
+                            String details = input.substring(9);
+                            int delimiter = details.indexOf("/");
+                            String task = details.substring(0, delimiter);
+                            String time = details.substring(delimiter + 4);
+                            Deadline deadline = new Deadline(task, time);
+                            tasks.add(deadline);
+                            System.out.println("Got it. I've added this task:\n" + deadline.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        }
+                }
+                input = scanner.nextLine();
+            } catch(Exception e) {
+                String[] inputWords = input.split(" ");
+                if (inputWords.length == 1) {
+                    switch(inputWords[0]) {
+                        case "todo":
+                            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                            input = scanner.nextLine();
+                            break;
+                        case "deadline": //WRONG
+                            System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                            input = scanner.nextLine();
+                            break;
+                        case "event": //WRONG
+                            System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+                            input = scanner.nextLine();
+                            break;
+                        case "done":
+                            System.out.println("☹ OOPS!!! Which task did you complete?");
+                            input = scanner.nextLine();
+                            break;
+                        default:
+                            System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                            input = scanner.nextLine();
                     }
-                    break;
-                default:
-                    if (input.length() >= 6 && input.substring(0, 5).equals("done ")) {
-                        String number = input.substring(5);
-                        int index = Integer.parseInt(number);
-                        tasks.get(index-1).markAsDone();
-                        System.out.println("Nice! I've marked this task as done:\n" + tasks.get(index-1).getStatusIcon() + tasks.get(index-1).getName());
-                    }
-                    else if (input.length() >= 10 && input.substring(0, 9).equals("deadline ")){
-                        String details = input.substring(9);
-                        int delimiter = details.indexOf("/");
-                        String task = details.substring(0, delimiter);
-                        String time = details.substring(delimiter+4);
-                        Deadline deadline = new Deadline(task, time);
-                        tasks.add(deadline);
-                        System.out.println("Got it. I've added this task:\n" + deadline.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
-                    }
-                    else if (input.length() >= 7 && input.substring(0, 6).equals("event ")) {
-                        String details = input.substring(6);
-                        int delimiter = details.indexOf("/");
-                        String task = details.substring(0, delimiter);
-                        String time = details.substring(delimiter+4);
-                        Event event = new Event(task, time);
-                        tasks.add(event);
-                        System.out.println("Got it. I've added this task:\n" + event.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
-                    }
-                    else {
-                        String details = input.substring(5);
-                        Todo todo = new Todo(details);
-                        tasks.add(todo);
-                        System.out.println("Got it. I've added this task:\n" + todo.toString() + "\nNow you have " + tasks.size() + " tasks in the list.");
-                    }
+                }
+                else {
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    input = scanner.nextLine();
+                }
+
             }
-            input = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
         return;
