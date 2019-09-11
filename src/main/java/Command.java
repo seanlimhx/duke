ef0a67196a837eeb87a25ecbfd7ddda9d98492d6
Geek <ygeek@gmail.com>
2019-09-11 23:55:41 +0800

    Added Javadoc to Command class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,20 +1,43 @@
+/**
+ * A Command abstract class that gives the basic properties and methods of a command
+ *
+ * @author Sean
+ *
+ */
+
 public abstract class Command {
     protected boolean isExit = false;
     private String details;
 
+    /**
+     * Default Command constructor
+     * @param details
+     */
     public Command(String details) {
         this.details = details;
     }
 
+    /**
+     * Return details of a Command
+     * @return
+     */
     public String getDetails() {
         return this.details;
     }
 
-    
 
+    /**
+     * Return a boolean of whether the program should exit
+     * @return
+     */
     public boolean isExit() {
         return this.isExit;
     }
 
+    /**
+     * Carry out the respective function of the command
+     * @param tasks
+     * @param storage
+     */
     public abstract void execute(TaskList tasks, Storage storage);
 }
